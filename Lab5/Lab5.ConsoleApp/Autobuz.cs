4faf4b330b81e43778c9b0c64927e1d3f1b26820
FerentClaudiu <32581324+FerentClaudiu@users.noreply.github.com>
2017-11-13 22:59:26 +0200

    tema1.2
    Adaugare tema

---
+++ Lab5/Lab5.ConsoleApp/Autobuz.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.ConsoleApp
+{
+    class Autobuz : Autovehicul
+    {
+        public Autobuz() : base()
+        {
+
+        }
+        public override void GetFromInput()
+        {
+            base.GetFromInput();
+            Console.WriteLine("introduceti tipul de autovehicul:");
+            Tipul = Console.ReadLine();
+        }
+    }
+}
---
+++ Lab5/Lab5.ConsoleApp/Motocicleta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5.ConsoleApp
+{
+    class Motocicleta : Autovehicul
+    {
+        public Motocicleta(string marca, string model, int numarroti, string tipul, int numarusi) : base(marca, model, numarroti, tipul, numarusi)
+        {
+
+        }
+        public override void GetFromInput()
+        {
+            base.GetFromInput();
+            Console.WriteLine("introduceti tipul de autovehicul:");
+            Tipul = Console.ReadLine();
+        }
+    }
+   
+}
