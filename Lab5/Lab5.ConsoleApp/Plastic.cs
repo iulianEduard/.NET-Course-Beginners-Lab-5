52b138dbb1ee81af52468153e181fde12dfe7d52
FerentClaudiu <32581324+FerentClaudiu@users.noreply.github.com>
2017-11-15 23:40:46 +0200

    obiecte1.2
    adaugare Clasa Plastic

--- Lab5/Lab5.ConsoleApp/Plastic.cs
+++ Lab5/Lab5.ConsoleApp/Plastic.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace Lab5.ConsoleApp
 {
-    class Class1
+    public class Plastic
+    {
+        public string Producator { get; set; }
+
+        public string Culoare { get; set; }
+
+        public int Butoane { get; set; }
+
+        
+
+        public Plastic()
+        {
+
+        }
+
+        public Plastic(string producator, string culoare, int butoane, )
+        {
+            Producator = producator;
+            Culoare = culoare;
+            Butoane = butoane;
+            
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Plastic {Producator} {Culoare}");
+            
+            Console.WriteLine($"Butoane: {Butoane}");
+            
+        }
+
+        
+    }
+
+    public class Mouse : Plastic
     {
     }
+
+    public class Toshiba : Plastic
+    { 
+        public Toshiba(string producator, string culoare, int butoane) : base(producator,culoare,butoane)
+        {
+        }
+
+        
+
+        }
 }
